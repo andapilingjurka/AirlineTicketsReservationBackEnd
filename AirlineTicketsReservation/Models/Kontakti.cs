35cb4805639951e00cc103c82536e84f69ad981c
VeneraGoxhufi <venera.goxhufi12@gmail.com>
2024-01-07 01:47:10 +0100

    Krijimi i klases "Kontakti" ne Models

---
+++ AirlineTicketsReservation/Models/Kontakti.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AirlineTicketsReservation.Models
+{
+    public class Kontakti
+    {
+        [Key]
+        public int KontaktID { get; set; }
+        public string Emri { get; set; }
+        public string Emaili { get; set; }
+        public string NumriTelefonit { get; set; }
+        public string Mesazhi { get; set; }
+
+
+    }
+}
