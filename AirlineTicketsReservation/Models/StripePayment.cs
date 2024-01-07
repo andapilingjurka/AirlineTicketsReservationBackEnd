0a2b1d83c52efce62575be90ada3a8d25ee69401
VeneraGoxhufi <venera.goxhufi12@gmail.com>
2024-01-07 02:27:44 +0100

    Krijimi i klases 'StripePayment'

---
+++ AirlineTicketsReservation/Models/StripePayment.cs
@@ -0,0 +1,14 @@
+﻿namespace AirlineTicketsReservation.Models
+{
+    public record StripePayment(
+         string CardNumber,
+        string ExpirationYear,
+        string ExpirationMonth,
+        string Cvc,
+
+
+    string Email,
+        string Currency,
+        long Amount,
+        string PaymentId);
+}
