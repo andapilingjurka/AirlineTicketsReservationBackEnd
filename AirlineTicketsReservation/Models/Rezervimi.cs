06e829ef99067f1bc975dacff61fa21e74e97b55
Dea <dk61403@ubt-uni.net>
2024-01-08 01:37:09 +0100

    Rezervimi Model

---
+++ AirlineTicketsReservation/Models/Rezervimi.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AirlineTicketsReservation.Models
+{
+    public class Rezervimi
+    {
+        [Key]
+
+        public int Id { get; set; }
+        public string Klasi { get; set; }
+        public long Cmimi { get; set; }
+        public string Currency { get; set; }
+        public int FluturimiId { get; set; }
+        public Boolean Kthyese { get; set; }
+
+        public DateTimeOffset Data_e_Rezervimit { get; set; }
+        public DateTime? Data_e_Kthimit = null;
+
+        public Fluturimi Fluturimi { get; set; }
+    }
+}
