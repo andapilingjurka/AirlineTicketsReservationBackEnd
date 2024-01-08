583af798b90014c8bc55bc1f4b03652dda0298e0
Dea <dk61403@ubt-uni.net>
2024-01-08 01:40:56 +0100

    DbSet Rezervimi

--- AirlineTicketsReservation/Data/ApplicationDbContext.cs
+++ AirlineTicketsReservation/Data/ApplicationDbContext.cs
@@ -18,6 +18,7 @@ namespace AirlineTicketsReservation.Data
         public DbSet<Aeroplani> Aeroplani { get; set; }
         public DbSet<Fluturimi>Fluturimet { get; set; }
 
+        public DbSet<Rezervimi> Rezervimet { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -30,19 +31,22 @@ namespace AirlineTicketsReservation.Data
             .OnDelete(DeleteBehavior.Restrict);
           
             modelBuilder.Entity<Fluturimi>()
-          .HasOne(p => p.Aeroplani)
-          .WithMany()
-          .HasForeignKey(p => p.AeroplaniId) //Foreign Key
-          .OnDelete(DeleteBehavior.Restrict);
-
-
-
+              .HasOne(p => p.Aeroplani)
+              .WithMany()
+              .HasForeignKey(p => p.AeroplaniId) //Foreign Key
+              .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder.Entity<Fluturimi>()
                 .HasOne(cf => cf.Qyteti)
                 .WithMany()
                 .HasForeignKey(cf => cf.QytetiId)
                  .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Rezervimi>()
+               .HasOne(p => p.Fluturimi)
+               .WithMany()
+               .HasForeignKey(p => p.FluturimiId) //Foreign Key
+               .OnDelete(DeleteBehavior.Restrict);
 
         }
     }
