eeb69558c67d339d89ebd54c23b8a32a4f78e1d7
VeneraGoxhufi <venera.goxhufi12@gmail.com>
2024-01-15 00:24:41 +0100

    Migrimi per Kontakt dhe Injektimi i databazes ne Program.cs

--- TicketsReservation/Model/Kontakti.cs
+++ TicketsReservation/Model/Kontakti.cs
@@ -8,7 +8,6 @@ namespace TicketsReservation.Model
         public int KontaktID { get; set; }
         public string Emri { get; set; }
         public string Emaili { get; set; }
-        public string NumriTelefonit { get; set; }
         public string Mesazhi { get; set; }
 
 
--- TicketsReservation/Program.cs
+++ TicketsReservation/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using TicketsReservation.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,8 +10,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDBConnectionn"));
+    }
+    );
+
 var app = builder.Build();
 
+app.UseCors(policy => policy.AllowAnyHeader()
+                               .AllowAnyMethod()
+                               .SetIsOriginAllowed(origin => true)
+                               .AllowCredentials()
+
+                               );
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
